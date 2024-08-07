b80dd6966c008020e3528ae814aa7afcdfb1f44f
Chadd Poggenpoel <chadd@cubicit.co.za>
2024-08-08 00:03:05 +0200

    0.0.1

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,13 +7,20 @@ require("dotenv").config({
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `rj-tours`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.rjtours.tld`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss",
+  plugins: [
+    {
+      resolve: `gatsby-source-custom`,
+      options: {
+        apiKey: process.env.GATSBY_PAGECLIP_API_KEY,
+      },
+    },
+    "gatsby-plugin-postcss",
     // "gatsby-plugin-google-gtag", 
     "gatsby-plugin-image", "gatsby-plugin-sitemap", {
       resolve: 'gatsby-plugin-manifest',
